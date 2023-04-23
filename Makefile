emersion/hello-wayland

commit f6a8203309977af03cda94765dd61367c189bea6

build: fix cross-compilation with pkg-config
Conventionally, when cross-compiling, PKG_CONFIG is set to the
prefixed pkg-config wrapper for the platform being built for.

Makefile
@@ -1,7 +1,9 @@
-WAYLAND_FLAGS = $(shell pkg-config wayland-client --cflags --libs)
-WAYLAND_PROTOCOLS_DIR = $(shell pkg-config wayland-protocols --variable=pkgdatadir)
-WAYLAND_SCANNER = $(shell pkg-config --variable=wayland_scanner wayland-scanner)
 CFLAGS ?= -std=c11 -Wall -Wextra -Werror -Wno-unused-parameter -g
+PKG_CONFIG ?= pkg-config
+
+WAYLAND_FLAGS = $(shell $(PKG_CONFIG) wayland-client --cflags --libs)
+WAYLAND_PROTOCOLS_DIR = $(shell $(PKG_CONFIG) wayland-protocols --variable=pkgdatadir)
+WAYLAND_SCANNER = $(shell $(PKG_CONFIG) --variable=wayland_scanner wayland-scanner)
 
 XDG_SHELL_PROTOCOL = $(WAYLAND_PROTOCOLS_DIR)/stable/xdg-shell/xdg-shell.xml
 

commit 2bbb94ec5c99fb189a226e59bdc6f0b5a0187748

build: don't use $PKG_CONFIG for wayland-scanner
I made a mistake in the previous commit — $PKG_CONFIG should not be
used to find wayland-scanner, because when cross-compiling that'll
look for the cross-compiled wayland-scanner.  I missed this because I
tested by compiling from glibc to musl on the same architecture, so I
was testing a special case where my system could actually execute the
cross-compiled wayland-scanner.

Fixes: f6a8203 ("build: fix cross-compilation with pkg-config")

Makefile
@@ -1,9 +1,12 @@
 CFLAGS ?= -std=c11 -Wall -Wextra -Werror -Wno-unused-parameter -g
 PKG_CONFIG ?= pkg-config
 
+# Host deps
 WAYLAND_FLAGS = $(shell $(PKG_CONFIG) wayland-client --cflags --libs)
 WAYLAND_PROTOCOLS_DIR = $(shell $(PKG_CONFIG) wayland-protocols --variable=pkgdatadir)
-WAYLAND_SCANNER = $(shell $(PKG_CONFIG) --variable=wayland_scanner wayland-scanner)
+
+# Build deps
+WAYLAND_SCANNER = $(shell pkg-config --variable=wayland_scanner wayland-scanner)
 
 XDG_SHELL_PROTOCOL = $(WAYLAND_PROTOCOLS_DIR)/stable/xdg-shell/xdg-shell.xml
 